31f926dadddd6ce06f48f94eba3784be3adef552
sarniraula <sar.niraula162@gmail.com>
2026-04-01 20:12:01 +0545

    Revert "user login, register feature implemented with firebase."
    This reverts commit c0efcced28c026cb6cabc7c8c0adea3bc925fb57.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,10 @@
-import { useColorScheme } from '@/hooks/use-color-scheme';
-import { useAuthStore } from '@/store/authStore';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Redirect, Stack, useRootNavigationState, useSegments } from 'expo-router';
+import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 import '../global.css';
+
+import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -12,27 +12,11 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const { isAuthenticated } = useAuthStore();
-  const segments = useSegments();
-  const navigationState = useRootNavigationState();
-
-  if (!navigationState?.key) return null; // wait until ready
-
-  const inAuthGroup = segments[0] === '(auth)';
-
-  if (!isAuthenticated && !inAuthGroup) {
-    return <Redirect href="/(auth)/login" />;
-  }
-
-  if (isAuthenticated && inAuthGroup) {
-    return <Redirect href="/(tabs)" />;
-  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
